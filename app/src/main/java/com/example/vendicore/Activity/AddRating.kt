f477a4291ae452084563620af43efd99686f53a0
Ashen_IT21286278 <99505510+IT21286278@users.noreply.github.com>
2024-10-08 02:12:28 +0530

    UI created to add a new vendor rating

--- app/src/main/java/com/example/vendicore/Activity/AddRating.kt
+++ app/src/main/java/com/example/vendicore/Activity/AddRating.kt
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.vendicore.R
 
-class IntroActivity : AppCompatActivity() {
+class AddRating : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_intro)
+        setContentView(R.layout.activity_add_rating)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.addRating)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
